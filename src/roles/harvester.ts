211db131e08259ca230b699abec288025104e6b6
Tobias Pfeiffer <pragtob@gmail.com>
2020-12-29 22:48:23 +0100

    Store in containers? But something is weird

--- src/roles/harvester.ts
+++ src/roles/harvester.ts
@@ -5,7 +5,7 @@ export default function run(creep: Creep): void {
         harvestSource(creep);
     }
     else {
-        var structures = creep.room.find(FIND_STRUCTURES, {
+        let structures = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_EXTENSION ||
                     structure.structureType == STRUCTURE_SPAWN ||
@@ -17,6 +17,21 @@ export default function run(creep: Creep): void {
             if(creep.transfer(structures[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(structures[0], {visualizePathStyle: {stroke: '#ffffff'}});
             }
+        }
+        else {
+            // hack because the type system doesn't let me include it above?
+            let containers = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+
+            if(containers.length > 0) {
+                if(creep.transfer(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+        }
        }
     }
 }
